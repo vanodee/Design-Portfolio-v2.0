611182e2d4f25c82dc8f0e8aa56502c6b118c27f
vanodee <vanodee007@gmail.com>
2025-11-05 14:06:53 +0100

    [i] next/sanity

---
+++ lib/sanity.client.ts
@@ -0,0 +1,16 @@
+// lib/sanity.client.ts
+import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const sanityConfig = {
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2025-01-01", // use current date or a stable date string
+  useCdn: true,
+};
+
+export const client = createClient(sanityConfig);
+export const builder = imageUrlBuilder(client);
+export function urlFor(source: any) {
+  return builder.image(source);
+}
