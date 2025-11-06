cf654e47a63f942e5794b9cbd817030fea35c3b4
vanodee <vanodee007@gmail.com>
2025-11-07 00:38:45 +0100

    [t] Setup font in layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({ 
+  subsets: ['latin'], 
+  weight: ['400','600'], 
+  variable: '--font-outfit' 
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={outfit.variable}>
         {children}
       </body>
     </html>
