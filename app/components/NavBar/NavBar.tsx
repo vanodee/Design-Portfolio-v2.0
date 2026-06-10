f8296ae7ef4c51c4ded339380fee2fc125ead0a1
vanodee <vanodee007@gmail.com>
2026-06-10 22:55:08 +0100

    [t] Updated Resume

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -14,7 +14,7 @@ export const navItems = [
   { name: 'Contact', path: '/contact' },
 ]
 
-export const myResume = "https://drive.google.com/file/d/191cebUAyLPtZY_4VGr2kEpw64V5nOoCV/view?usp=sharing";
+export const myResume = "https://drive.google.com/file/d/1rO_9eMv44jXV6cMHTiL6f55bTd9uBGQ4/view?usp=sharing";
 
 
 export default function NavBar() {
